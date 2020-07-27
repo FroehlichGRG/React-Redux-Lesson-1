5557e91c9bb08f194adbf3066169706bcb1e8a6a
FroehlichGRG <41758938+FroehlichGRG@users.noreply.github.com>
2020-07-27 16:07:55 +0200

    hello world

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function TodoItem() {
+  return <span>Drink Coffee</span>;
+}
+
+ReactDOM.render(<TodoItem />, document.querySelector("#root"));
